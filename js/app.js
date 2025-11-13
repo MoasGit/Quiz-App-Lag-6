4e1e03c46009c51970a5fb9565dceedcd3f2d5a9
Jesper Reijs <jesper.reijs@chasacademy.segit config --global user.emailgit config --global user.email jesper.reijs@chasacademy.se>
2025-11-13 13:30:09 +0100

    fixed some buggs

--- js/app.js
+++ js/app.js
@@ -265,19 +265,6 @@ function displayQuiz(themes) {
   start();
 }
 
-function createSnowflakes() {
-    for (let i = 0; i < 100; i++) {
-        const snowflake = document.createElement('div');
-        snowflake.className = 'snowflake';
-        snowflake.innerHTML = '❄';
-        snowflake.style.left = Math.random() * 100 + '%';
-        snowflake.style.animationDuration = (Math.random() * 3 + 2) + 's';
-        snowflake.style.animationDelay = Math.random() * 5 + 's';
-        snowflake.style.fontSize = (Math.random() * 10 + 10) + 'px';
-        document.body.appendChild(snowflake);
-    }
-}
-
 ///NOLLSTÄLLER QUIZZET OCH GÅR TILLBAKS TILL TEMAVAL-CONTAINERN
 restartBtn.addEventListener("click", function () {
   resultsView.classList.remove("active");
